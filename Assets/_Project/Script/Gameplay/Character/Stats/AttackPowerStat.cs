a427c3f3eaecd4ef64121e24e635e50f666c970d
Rhea <rhealimkinglam@gmail.com>
2025-04-10 22:36:21 +0800

    Added scripts for stat scriptable object

---
+++ Assets/_Project/Script/Gameplay/Character/Stats/AttackPowerStat.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace NF.Main.Gameplay.Character.Stats
+{
+    [CreateAssetMenu(fileName = "AttackPowerStat", menuName = "ScriptableObject/Stats/Attack Power Stat")]
+    public class AttackPowerStat : Stat
+    {
+    }
+}
---
+++ Assets/_Project/Script/Gameplay/Character/Stats/SpeedStat.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace NF.Main.Gameplay.Character.Stats
+{
+    [CreateAssetMenu(fileName = "SpeedStat", menuName = "ScriptableObject/Stats/Speed Stat")]
+    public class SpeedStat : Stat
+    {
+    }
+}
