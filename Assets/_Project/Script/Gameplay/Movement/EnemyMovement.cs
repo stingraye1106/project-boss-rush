782c8d979fe76d0f2c14e56b2c6a65ec25e1b765
Rhea <rhealimkinglam@gmail.com>
2025-04-15 22:07:10 +0800

    Implement initial player chase AI behavior

---
+++ Assets/_Project/Script/Gameplay/Movement/EnemyMovement.cs
@@ -0,0 +1,71 @@
+using NF.Main.Core;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace NF.Main.Gameplay.Movement
+{
+    public class EnemyMovement : MonoExt, IMovement
+    {
+        [TabGroup("Movement Settings")][SerializeField] private NavMeshAgent _navMeshAgent;
+        [TabGroup("Movement Settings")][SerializeField] private bool _canMove;
+        [TabGroup("Movement Settings")][SerializeField] private float _speedMultiplier;
+
+        private float _speed;
+        private Vector3 _direction;
+
+
+        public float Speed { get => _speed; set => _speed = value; }
+        public float SpeedMultiplier { get => _speedMultiplier; set => _speedMultiplier = value; }
+        public Vector3 Direction { get => _direction; set => _direction = value; }
+        public bool CanMove { get => _canMove; set => _canMove = value; }
+
+
+
+
+        public override void Initialize(object data = null)
+        {
+            base.Initialize(data);
+            _canMove = true;
+            UpdateMovementSpeed();
+        }
+
+        private void Update()
+        {
+            if (!_canMove)
+            {
+                StopMovement();
+            } else
+            {
+                StartMovement();
+            }
+            SetDestination();
+
+            if  (_navMeshAgent.isStopped)
+            {
+                Debug.Log("stopped");
+            }
+        }
+
+        private void StartMovement()
+        {
+            _navMeshAgent.isStopped = false;
+        }
+
+        private void StopMovement()
+        {
+            _navMeshAgent.isStopped = true;
+        }
+
+        private void SetDestination()
+        {
+            _navMeshAgent.SetDestination(_direction);
+        }
+
+        private void UpdateMovementSpeed()
+        {
+            _navMeshAgent.speed = _speed * _speedMultiplier;
+        }
+    }
+}
+
