94145208b8c8d1afa3cf9b49193b2b9830baa8c2
Rhea <rhealimkinglam@gmail.com>
2025-04-18 20:19:44 +0800

    Update take damage method to implement changing of game states if player or enemy health has reached zero

--- Assets/_Project/Script/Gameplay/Character/EnemyCharacter.cs
+++ Assets/_Project/Script/Gameplay/Character/EnemyCharacter.cs
@@ -9,32 +9,40 @@ namespace NF.Main.Gameplay.Character
         public Transform ChaseTarget { get; set; }
         public float AttackRange => _attackRange;
 
-        public override void Initialize(object data = null)
-        {
-            base.Initialize(data);
-        }
-
+        // Enemy will pursue the selected chase target.
         private void Update()
         {
             _movement.Direction = ChaseTarget.position;
         }
 
+        // Enemy will use the basic attack during basic attack state.
         public override void Attack()
         {
             _basicAttackAbility.Use(gameObject);
         }
 
+        // Enables movement of the enemy.
         public override void Move(Vector3 direction)
         {
             _movement.CanMove = true;
         }
 
+        // Handles taken damage and transition to enemy death state.
         public override void TakeDamage(float damage)
         {
             _health.CurrentValue -= damage;
             Debug.Log($"Enemy took {damage} damage! Current enemy health: {_health.CurrentValue}");
+
+            if (_health.CurrentValue <= 0) 
+            {
+                // Play death animation
+
+                // Set game state to victory.
+                GameManager.Instance.GameState = GameState.Victory;
+            }
         }
 
+        // Stops movement. This is for states that don't require the enemy to chase the player.
         public override void StopMovement()
         {
             _movement.CanMove = false;
--- Assets/_Project/Script/Gameplay/Character/PlayerCharacter.cs
+++ Assets/_Project/Script/Gameplay/Character/PlayerCharacter.cs
@@ -4,21 +4,34 @@ namespace NF.Main.Gameplay.Character
 {
     public class PlayerCharacter : ACharacter
     {
+        // Uses the basic attack ability.
         public override void Attack()
         {
             _basicAttackAbility.Use(gameObject);
         }
 
+        // Injects the player input into the movement controller.
         public override void Move(Vector3 direction)
         {
             _movement.Direction = direction;
         }
 
+        // Handles damage taking and transition to player death state.
         public override void TakeDamage(float damage)
         {
             _health.CurrentValue -= damage;
+            Debug.Log($"Player took {damage} damage! Current player health: {_health.CurrentValue}");
+
+            if (_health.CurrentValue <= 0 )
+            {
+                // Play death animation
+
+                // Set game state to Game Over.
+                GameManager.Instance.GameState = GameState.GameOver;
+            }
         }
 
+        // Will stop player movement. This is for idle states or other states that don't need movement input.
         public override void StopMovement()
         {
             _movement.Direction = Vector3.zero;
