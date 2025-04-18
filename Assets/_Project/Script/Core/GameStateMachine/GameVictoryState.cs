fe50fae034d7b178c69bb426656706c1c51b4ad0
Rhea <rhealimkinglam@gmail.com>
2025-04-18 22:40:15 +0800

    Updated game over and victory states

--- Assets/_Project/Script/Core/GameStateMachine/GameVictoryState.cs
+++ Assets/_Project/Script/Core/GameStateMachine/GameVictoryState.cs
@@ -1,19 +1,32 @@
 using NF.Main.Gameplay;
+using NF.Main.Gameplay.Character;
+using NF.Main.UI;
 using UnityEngine;
 
 namespace NF.Main.Core.GameStateMachine
 {
     public class GameVictoryState : GameBaseState
     {
-        public GameVictoryState(GameManager gameManager, GameState gameState) : base(gameManager, gameState)
+        private PlayerInputReader _inputReader;
+        private PlayerCharacter _player;
+        private EnemyCharacter _enemy;
+
+        public GameVictoryState(GameManager gameManager, GameState gameState, PlayerInputReader inputReader, PlayerCharacter player, EnemyCharacter enemy) : base(gameManager, gameState)
         {
+            _inputReader = inputReader;
+            _player = player;
+            _enemy = enemy;
         }
 
         public override void OnEnter()
         {
             base.OnEnter();
             Debug.Log("Win state");
+
+            _player.StopMovement();
+            _enemy.StopMovement();
+            _inputReader.DisablePlayerActionMap();
+            UIManager.Instance.ShowVictoryScreen();
         }
     }
 }
-
